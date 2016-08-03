f9efc017874b46130789e748d40e2952af8ab81c
leon2796 <leon2796@gmail.com>
2016-08-03 13:48:03 +0300

    first commit

---
+++ js/common.js
@@ -0,0 +1,60 @@
+$(function() {
+
+	//SVG Fallback
+	if(!Modernizr.svg) {
+		$("img[src*='svg']").attr("src", function() {
+			return $(this).attr("src").replace(".svg", ".png");
+		});
+	};
+
+	//E-mail Ajax Send
+	//Documentation & Example: https://github.com/agragregra/uniMail
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+	//Chrome Smooth Scroll
+	try {
+		$.browserSelector();
+		if($("html").hasClass("chrome")) {
+			$.smoothScroll();
+		}
+	} catch(err) {
+
+	};
+
+	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
+	
+});
+
+$(window).load(function() {
+
+	$(".loader_inner").fadeOut();
+	$(".loader").delay(400).fadeOut("slow");
+
+});
+
+$(window).scroll(function() {
+	var girl =$(".face-normal");
+
+	var posY1 = $(window).scrollTop();
+	if(posY1 < 930) {
+		//man.css("top", -1*(posY1 - 1180));
+		girl.css("height", (930 - posY1));
+	}
+	else if (posY1 > 930) {
+		girl.css("height", 0);
+	}
+});
